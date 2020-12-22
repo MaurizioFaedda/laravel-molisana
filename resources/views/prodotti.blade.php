91a98a1af912d7a6d7c8cd8876c4f21ccafeab9d
Maurizio <maurizio.faedda1@gmail.com>
2020-12-22 17:16:08 +0100

    Printed data in products

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -3,5 +3,55 @@
 @section('title', 'Prodotti - La Molisana')
 
 @section('content')
-    <h1>Prodotti</h1>
+    <div class="section-prodotti" style="background-image: url({{asset('images/sfondo-la-molisana.jpg')}});">
+        <div class="container">
+
+            <div class="cards-box">
+
+                <h1> Le lunghe </h1>
+                <div class="card-box-type">
+                    @foreach ($pasta as $value)
+                        @if ($value['tipo'] === 'lunga')
+                            <div class="card">
+                                <div class="img-pasta">
+                                    <img src="{{$value["src"]}}" alt="">
+                                </div>
+
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+
+                <h1> Le corte </h1>
+                <div class="card-box-type">
+                    @foreach ($pasta as $value)
+                        @if ($value['tipo'] === 'corta')
+                            <div class="card">
+                                <div class="img-pasta">
+                                    <img src="{{$value["src"]}}" alt="">
+                                </div>
+
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+
+                <h1> Le cortissime </h1>
+                <div class="card-box-type">
+                    @foreach ($pasta as $value)
+                        @if ($value['tipo'] === 'cortissima')
+                            <div class="card">
+                                <div class="img-pasta">
+                                    <img src="{{$value["src"]}}" alt="">
+                                </div>
+
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 @endsection
